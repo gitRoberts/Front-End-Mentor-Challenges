b76b57c8d35dde3bfe9905d3467f37b3ffc3e338
Bryan <brober29@asu.edu>
2022-12-01 18:10:48 -0600

    Update Coding bootcamp js best practices

--- Coding Bootcamp Testimonial/script.js
+++ Coding Bootcamp Testimonial/script.js
@@ -1,34 +1,39 @@
 let prevButton = document.querySelector(".left-btn");
 let nextButton = document.querySelector(".right-btn");
-let profileImageJohn = document.querySelector(".john-image");
-let profileImageTanya = document.querySelector(".tanya-image");
+let profileImage = document.querySelector(".primary-image");
 let testimonial = document.querySelector("#testimonial");
 let fullname = document.querySelector("#fullname");
 let position = document.querySelector("#position-title");
+let people;
+let index = 0;
 
-function changeImage() {
-    if(profileImageJohn.classList.contains("hidden")) {
-        profileImageJohn.classList.remove("hidden");
-        profileImageTanya.classList.add("hidden");
-        changeTestimonial(true);
-    } else {
-        profileImageJohn.classList.add("hidden");
-        profileImageTanya.classList.remove("hidden");
-        changeTestimonial(false);
-    }
+async function getData() {
+    let response = await fetch("./data.json");
+    let data = await response.json();
+    people = data;
+    draw(index);
 }
 
-function changeTestimonial(input) {
-    if(input) {
-        testimonial.textContent = "“ If you want to lay the best foundation possible I’d recommend taking this course. The depth the instructors go into is incredible. I now feel so confident about starting up as a professional developer. ”";
-        fullname.textContent = "John Tarkpor";
-        position.textContent = "Junior Front-end Developer";
-    } else {
-        testimonial.textContent = "“ I’ve been interested in coding for a while but never taken the jump, until now. I couldn’t recommend this course enough. I’m now in the job of my dreams and so excited about the future. ”";
-        fullname.textContent = "Tanya Sinclair";
-        position.textContent = "UX Engineer";
-    }
+function draw(input) {
+    profileImage.src = `${people[input].img}`;
+    fullname.textContent = `${people[input].name}`;
+    position.textContent = `${people[input].occupation}`;
+    testimonial.textContent = `" ${people[input].testimonial} "`;
 }
 
-prevButton.addEventListener("click", changeImage);
-nextButton.addEventListener("click", changeImage);
+prevButton.addEventListener("click", () => {
+    if(index < 0 ) {
+        index = people.length - 1;
+    } else if(index > people.length - 1) {
+        index = 0;
+    } 
+    draw(index--);
+});
+nextButton.addEventListener("click", () => {
+    if(index > people.length - 1) {
+        index = 0;
+    }
+    draw(index++);
+});
+
+getData();
